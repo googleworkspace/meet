5c6c840545d5c05fca6c4d79b41034238de533af
Teddy Ward <edwinward@google.com>
2024-08-21 16:25:47 -0400

    feat: Write hello world add-on and deploy to GitHub Pages (#13)
    * feat: Write hello world add-on and deploy to GitHub Pages
    
    * fix: main stage URL
    
    * fix: Clean up sample set up
    
    * fix: DS Store capitalization
    
    * refactor: export style of setUpAddon function
    
    Co-authored-by: Aziz Javed <azizj1@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Aziz Javed <azizj1@users.noreply.github.com>

---
+++ addons-web-sdk/samples/hello-world/README.md
@@ -0,0 +1,3 @@
+# Hello, World: The Add-on
+
+This is approximately the simplest possible add-on. The only requirements to get started here are npm and GitHub. The intent of publishing this add-on is to demonstrate how easy it is to get started with rendering custom content using an add-on. Please see other samples at <https://github.com/googleworkspace/meet/tree/main/addons-web-sdk/samples> to learn more advanced functionality!
---
+++ addons-web-sdk/samples/hello-world/src/index.js
@@ -0,0 +1,33 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { meet } from '@googleworkspace/meet-addons/meet.addons';
+
+// TODO: Make sure that you modify these constants, if you fork this!
+const CLOUD_PROJECT_NUMBER = "12345678910";
+const MAIN_STAGE_URL = "https://googleworkspace.github.io/meet/hello-world/MainStage.html"
+
+/**
+ * Prepares the Add-on Side Panel Client, and adds an event to launch the main
+ * stage when the main button is clicked.
+ */
+export async function setUpAddon() {
+  const session = await meet.addon.createAddonSession({
+    cloudProjectNumber: CLOUD_PROJECT_NUMBER,
+  });
+  sidePanelClient = await session.createSidePanelClient();
+  document.getElementById('start-activity').addEventListener('click', async () => {
+    await sidePanelClient.startCollaboration({ mainStageUrl: MAIN_STAGE_URL });
+  });
+}
