5cc003223350fd0b1a7fbfc87a51d0a4d51abe58
Teddy Ward <edwinward@google.com>
2024-08-28 15:27:51 -0500

    fix routing... again

--- addons-web-sdk/samples/hello-world-next-js/next.config.mjs
+++ addons-web-sdk/samples/hello-world-next-js/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/meet/hello-world-react-ts-vite',
+  basePath: '/meet/hello-world-next-js',
   distDir: '../dist/hello-world-next-js',
   output: 'export',
 };
