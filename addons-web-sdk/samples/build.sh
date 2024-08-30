88b21e34fca494304c5b5d01a3c05447ace0ae72
Teddy Ward <edwinward@google.com>
2024-08-30 14:25:55 -0500

    test: verify that using npm workspaces in this way will deploy

--- addons-web-sdk/samples/build.sh
+++
@@ -1,9 +0,0 @@
-for sample in ./*/
-do
-    if [ \"$sample\" != \"./dist\/\" ] && [ \"$sample\" != \"./node_modules\/\" ]; then
-        cd $sample;
-        echo \\n$sample;
-        npm run build;
-        cd ..;
-    fi
-done
