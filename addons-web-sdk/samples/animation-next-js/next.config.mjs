e14a9d282a723632003f7ef06331e7692516c88c
Teddy Ward <edwinward@google.com>
2024-09-06 17:22:33 -0400

    fix: redeploy with terms, privacy, and support at site root

--- addons-web-sdk/samples/animation-next-js/next.config.mjs
+++ addons-web-sdk/samples/animation-next-js/next.config.mjs
@@ -3,26 +3,6 @@ const nextConfig = {
   basePath: '/meet/animation-next-js',
   distDir: '../dist/animation-next-js',
   output: 'export',
-  async redirects() {
-    return [
-      // Basic redirect
-      {
-        source: '/terms',
-        destination: 'https://policies.google.com/terms',
-        permanent: true,
-      },
-      {
-        source: '/privacy',
-        destination: 'https://policies.google.com/privacy',
-        permanent: true,
-      },
-      {
-        source: '/support',
-        destination: 'https://support.google.com/',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
